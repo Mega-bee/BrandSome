8562a9d67c10b99303ba88585aadf70eeb60fbed
peter <patourtohme9@gmail.com>
2022-07-25 10:44:36 +0300

    category home pagee

--- lib/home_page/ui/state/homepage_success.dart
+++ lib/home_page/ui/state/homepage_success.dart
@@ -1,15 +1,12 @@
 import 'package:brandsome/abstracts/states/state.dart';
+import 'package:brandsome/categories_module/reponse/category_response.dart';
 import 'package:brandsome/categories_module/ui/widget/category_card.dart';
 import 'package:brandsome/home_page/model/category_model.dart';
 import 'package:brandsome/home_page/request/is_like.dart';
 import 'package:brandsome/home_page/ui/screen/homePage.dart';
 import 'package:flutter/material.dart';
-import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
-import '../../../categories_module/reponse/category_response.dart';
 import '../../../generated/l10n.dart';
 import '../../../posts_module/ui/widgets/post_card.dart';
-import '../../home_route.dart';
-import '../../request/userInterestsRequest.dart';
 import '../../response/home_page.dart';
 import '../widgets/main_cate_card.dart';
 import '../widgets/sub_service_card.dart';
@@ -59,14 +56,13 @@ class HomePageSuccess extends States {
     }
     screenState.refresh();
   }
-  List<int> serviceId = [];
-  setUserInterestsRequest request =
-  setUserInterestsRequest(id: []);
+
   @override
   Widget getUI(BuildContext context) {
-    final ItemScrollController itemScrollController = ItemScrollController();
-    final ItemPositionsListener itemPositionsListener =
-        ItemPositionsListener.create();
+
+//    final ItemScrollController itemScrollController = ItemScrollController();
+//    final ItemPositionsListener itemPositionsListener =
+//        ItemPositionsListener.create();
 
     return SingleChildScrollView(
       physics:
@@ -125,18 +121,11 @@ class HomePageSuccess extends States {
           alignment: AlignmentDirectional.topEnd,
           child: InkWell(
             onTap: () {
-              Navigator.pushNamed(context,
-                  HomeRoutes.CATEGORY_LIST_SCREEN).then((value) {
-                    value as List<ServiceModel>;
-                    for (var element in value) {
-                      serviceId.add(element.id??-1);
-
-                    }
-                    ///request
-                    request.id = serviceId;
-                    print("ServiceId:${serviceId}");
-
-                  });
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //       builder: (context) => SearchBarFilterScreen()),
+              // );
             },
             child: Padding(
               padding: const EdgeInsetsDirectional.only(
@@ -153,22 +142,16 @@ class HomePageSuccess extends States {
         const Divider(
           thickness: 3,
         ),
-        ScrollablePositionedList.builder(
-            itemScrollController: itemScrollController,
-            itemPositionsListener: itemPositionsListener,
+        ListView.builder(
             physics: const BouncingScrollPhysics(),
             shrinkWrap: true,
             itemCount: posthome.length,
             itemBuilder: (context, index) {
               return PostCard(
-                  isLogged: isLogged,
-                  onLikeClick: (like) {
-                    screenState.Islike(
-                        LikeRequest(
-                          isLike: like,
-                        ),
-                        posthome[index].id.toString());
-                  },
+                isLogged: isLogged,
+                onLikeClick: (like){
+                  screenState.Islike(LikeRequest(isLike: like,),posthome[index].id.toString());
+                },
                   onViewLikeTap: (id) {
                     screenState.goToLikes(id);
                   },
