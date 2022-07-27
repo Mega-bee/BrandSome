fbd8d189cd4fab8fdc78c12878887667c8bbebce
peter <patourtohme9@gmail.com>
2022-07-27 23:29:33 +0300

    revert first commit

--- lib/home_page/home_module.dart
+++ lib/home_page/home_module.dart
@@ -1,20 +1,15 @@
-
 import 'package:brandsome/abstracts/module/rout_module.dart';
 import 'package:brandsome/home_page/home_route.dart';
 import 'package:brandsome/home_page/ui/screen/homePage.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 
-import '../categories_module/ui/screen/categories_list_screen.dart';
-
 @injectable
 class HomeModule extends RoutModule {
   final HomePage _homePage;
-  final CategoryListScreen _categoryListScreen;
-
 
   HomeModule(
-    this._homePage,this._categoryListScreen,
+    this._homePage,
   ) {
     RoutModule.RoutesMap.addAll(getRoutes());
   }
@@ -22,7 +17,6 @@ class HomeModule extends RoutModule {
   Map<String, WidgetBuilder> getRoutes() {
     return {
       HomeRoutes.HOME_Screen: (context) => _homePage,
-      HomeRoutes.CATEGORY_LIST_SCREEN:(context)=>_categoryListScreen
     };
   }
 }
--- lib/home_page/home_route.dart
+++ lib/home_page/home_route.dart
@@ -1,5 +1,4 @@
 class HomeRoutes {
   static const HOME_Screen = '/home_screen';
-  static const CATEGORY_LIST_SCREEN = '/category_list';
 
 }
